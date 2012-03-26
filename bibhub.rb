ff828a921ef8ef08f78fbea6641432f2e18f96da
tanatana <tugusuke@gmail.com>
2012-03-26 16:45:19 +0900

    twitter認証とsessionの永続化をしました

--- bibhub.rb
+++ bibhub.rb
@@ -1,10 +1,16 @@
+$:.unshift File.dirname(__FILE__)
+
 require 'rubygems'
 require 'sinatra/base'
 require 'omniauth'
 require 'erb'
+require 'mongo_mapper'
+require 'models/user'
 
 class BibhubApp < Sinatra::Base
 
+  MongoMapper.database = "bibhub"
+  
   configure do
     use Rack::Session::Cookie, :secret => "change me"
     
@@ -17,13 +23,25 @@ class BibhubApp < Sinatra::Base
     provider :twitter,'P2ZQaY38pGcxwMgShg3rXQ','aUKFW4xzx2tJEkK1dxnsZZCqJhQzhFV8uHUTGxjUc'
   end
 
+  def get_user
+    @user ||= User.find_by_user_id(session[:user_id])
+  end
+
+
   get '/' do
     erb :index
   end
 
   get '/auth/:name/callback' do
     @auth = request.env['omniauth.auth']
+    session[:user_id] = @auth['uid']
+
+    @user = User.find_or_initialize_by_user_id(@auth['uid'])
+    @user.token = @auth['credentials']['token']
+    @user.secret = @auth['credentials']['secret']
+
+    @user.save
+
     erb :index2
   end
-  
 end
