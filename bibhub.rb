a11a6578f17a4089e13c07d87b3ed89791935756
tanatana <tugusuke@gmail.com>
2012-03-26 15:33:32 +0900

    auth実装しました

--- bibhub.rb
+++ bibhub.rb
@@ -1,14 +1,29 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'omniauth'
+require 'erb'
 
 class BibhubApp < Sinatra::Base
+
   configure do
+    use Rack::Session::Cookie, :secret => "change me"
+    
     set :logging, true
     set :dump_errors, true
     set :show_exceptions, true
   end
   
-  get '/' do
-    "bibtex"
+  use OmniAuth::Builder do
+    provider :twitter,'P2ZQaY38pGcxwMgShg3rXQ','aUKFW4xzx2tJEkK1dxnsZZCqJhQzhFV8uHUTGxjUc'
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/auth/:name/callback' do
+    @auth = request.env['omniauth.auth']
+    erb :index2
+  end
+  
 end
