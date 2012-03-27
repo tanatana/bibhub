fd1e125a7c01ccb6c6d9fbde2c79caf319792bdc
tanatana <tugusuke@gmail.com>
2012-03-27 14:07:23 +0900

    key :screen_name 追加

--- models/user.rb
+++ models/user.rb
@@ -2,7 +2,8 @@ class User
   include MongoMapper::Document
   key :user_id, String, :required => true 
   key :token, String
-  key :secret,String
+  key :secret, String
+  key :screen_name, String
 
   timestamps!
 end
